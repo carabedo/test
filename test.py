90b8eb4d4de10d3f0e325bfa8dd3e2c1e62650de
carabedo <carabedo@gmail.com>
2023-04-24 23:55:10 -0300

    v0.0233244

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ import requests as rq
 
 url='https://7pyngmccwa.execute-api.us-east-1.amazonaws.com/default/apitest?q=pan lactal'
 
-data=rq.get(url)
-print(data.json())
+data=rq.get(url).json()
+
+for x in data:
+    print(x)
